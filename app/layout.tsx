9ae1bbfdc9bceb2256bdd41821963e5a28dc5c96
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2026-03-17 16:01:42 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "AKWELD",
@@ -9,6 +10,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>{children}</body>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-EM9FBW342Z"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EM9FBW342Z');
+        `}
+      </Script>
     </html>
   );
 }
