4caaf61ced5e4ed80c96c595a7b8310c2e2edcaf
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2026-03-20 12:28:07 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-import HomePageContent from "@/src/components/HomePageContent";
-import SiteHeader from "@/src/components/SiteHeader";
-import SiteFooter from "@/src/components/SiteFooter";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <div className="topbar">
-        <div className="container">
-          <SiteHeader locale="en" />
-        </div>
-      </div>
-
-      <main>
-        <HomePageContent locale="en" />
-      </main>
-
-      <SiteFooter locale="en" />
-    </>
-  );
+  redirect("/en");
 }
