8dbd689bd69b2c5ad9590a2eea79495e0230ebb5
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2026-03-30 06:45:29 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/en/about",
+        destination: "/en",
+        permanent: true,
+      },
+      {
+        source: "/ru/about",
+        destination: "/ru",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
- 
