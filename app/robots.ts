0757c55062e6fa01c4ae4615d28f686b1a51ea7c
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2026-07-23 19:33:02 +0300

    Use www domain in robots metadata

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://akweldsteel.com";
+  const baseUrl = "https://www.akweldsteel.com";
 
   return {
     rules: {
